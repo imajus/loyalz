b6c3c5a6738cac1be26a8a79a70e56bcaacc3fc6
qutluq <kootlook.applications@gmail.com>
2024-08-28 15:48:39 +0500

    style: add mobile phone first layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-col gap-3 items-center justify-center h-[100dvh] w-screen">
-        {children}
+        <div className="w-full h-full sm:w-96 sm:h-[700px] bg-white sm:rounded-2xl flex flex-col justify-start items-center">
+          {children}
+        </div>
       </body>
     </html>
   );
