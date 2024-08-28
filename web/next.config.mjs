357962e0897830a05c9146abe0aceb0b082e28d0
qutluq <kootlook.applications@gmail.com>
2024-08-28 17:06:00 +0500

    chore: reconfigure tsconfig to include js files

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   // fixes wallet connect dependency issue https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
   webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
 };
