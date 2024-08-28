4f740717f0673ef0a9b367ffbb7cb888d0d4f566
qutluq <kootlook.applications@gmail.com>
2024-08-28 22:04:23 +0500

    feat: remove unneeded button on transations page

--- web/src/_pages/history/History.tsx
+++ web/src/_pages/history/History.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-
 import { transactions } from '@/mock/transactions';
-import { Button, MainWrapper } from '@/shared/components';
+import { MainWrapper } from '@/shared/components';
 
 import { HistoryItem } from './ui/HistoryItem';
 
 export const HistoryPage = () => {
-  const router = useRouter();
-
   return (
     <MainWrapper title="History" page="history">
       <div
@@ -19,9 +15,6 @@ export const HistoryPage = () => {
         {transactions.map((item) => (
           <HistoryItem key={`${item.date}-${item.tokenCompany}`} item={item} />
         ))}
-      </div>
-      <div className="flex items-center justify-center h-20 w-full">
-        <Button onClick={() => router.push('/wallet', { scroll: false })} title="Go back" />
       </div>
     </MainWrapper>
   );
