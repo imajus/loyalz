47ea0a578a4f5bb380bbfa5ee47ae466d7153f47
qutluq <kootlook.applications@gmail.com>
2024-08-30 21:35:44 +0500

    fix: build error

--- web/src/shared/components/require-auth/RequireAuth.tsx
+++ web/src/shared/components/require-auth/RequireAuth.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 
 import { useAppSelector } from '@/shared/store/hook';
 import { userSelector } from '@/shared/store/selector/user';
@@ -14,11 +14,16 @@ export const RequireAuth = ({ children }: PropTypes) => {
   const isAuthenticated = !!(currentUser.idToken || currentUser.oAuthAccessToken);
   const router = useRouter();
 
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push('/web3auth', { scroll: false });
+      toastError('Authorization requred');
+    }
+  }, [router, isAuthenticated]);
+
   if (isAuthenticated) {
     return children;
   }
 
-  toastError('Authorization requred');
-
-  router.push('web3auth', { scroll: false });
+  return null;
 };
