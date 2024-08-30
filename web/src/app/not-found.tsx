52b5d3fe7a159e1c697d0fd803b6233b457dbfeb
qutluq <kootlook.applications@gmail.com>
2024-08-30 10:45:25 +0500

    refactor: use button component on not-found page

--- web/src/app/not-found.tsx
+++ web/src/app/not-found.tsx
@@ -1,16 +1,12 @@
-import Link from 'next/link';
-
-import { MainWrapper } from '@/shared/components';
+import { Button, MainWrapper } from '@/shared/components';
 
 export default function NotFound() {
   return (
     <div className="w-full h-full sm:w-96 bg-white sm:rounded-2xl flex flex-col justify-start items-center">
       <MainWrapper title="Wallet" page="wallet">
         <div className="h-full w-full flex flex-col items-center justify-center gap-5">
-          <span className="text-black text-2xl font-bold p-3 rounded-md">Page not found</span>
-          <Link href="/" className="text-black text-3xl p-3 rounded-md border-2 border-black">
-            Return Home
-          </Link>
+          <span className="text-black text-3xl font-bold p-3 rounded-md">Page not found</span>
+          <Button href="/" title="Return Home" />
         </div>
       </MainWrapper>
     </div>
