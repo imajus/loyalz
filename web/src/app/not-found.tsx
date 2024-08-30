ef351b60c196dbe16094630d7e3ad3e9630bb572
qutluq <kootlook.applications@gmail.com>
2024-08-30 11:07:40 +0500

    feat: activate current page button in menu on not-found page

--- web/src/app/not-found.tsx
+++ web/src/app/not-found.tsx
@@ -1,9 +1,17 @@
+'use client';
+import { usePathname } from 'next/navigation';
+
 import { Button, MainWrapper } from '@/shared/components';
+import { pages } from '@/shared/const';
+import { Page } from '@/shared/types';
+import { removeLeadingTrailingSlashes } from '@/shared/utils';
 
 export default function NotFound() {
+  const slug = removeLeadingTrailingSlashes(usePathname()) as Page;
+  const page: Page = pages.includes(slug) ? slug : 'earn';
   return (
     <div className="w-full h-full sm:w-96 bg-white sm:rounded-2xl flex flex-col justify-start items-center">
-      <MainWrapper title="Wallet" page="wallet">
+      <MainWrapper title="Wallet" page={page}>
         <div className="h-full w-full flex flex-col items-center justify-center gap-5">
           <span className="text-black text-3xl font-bold p-3 rounded-md">Page not found</span>
           <Button href="/" title="Return Home" />
--- web/src/shared/utils/index.ts
+++ web/src/shared/utils/index.ts
@@ -1,2 +1,5 @@
 export const classNames = (...classes: Array<string | boolean>): string =>
   classes.filter(Boolean).join(' ');
+
+export const removeLeadingTrailingSlashes = (str: string): string =>
+  str?.replace(/^\/|\/$/g, '') || '';
