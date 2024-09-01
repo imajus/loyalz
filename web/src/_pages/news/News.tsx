e51c94e9282bcd79e422f479125f956da0c13d0f
qutluq <kootlook.applications@gmail.com>
2024-09-01 20:18:06 +0500

    feat: remove unneeded sections from news page

--- web/src/_pages/news/News.tsx
+++ web/src/_pages/news/News.tsx
@@ -1,14 +1,13 @@
 'use client';
 
 import { news } from '@/mock/news';
-import { Button, MainWrapper } from '@/shared/components';
-import { toastInfo } from '@/shared/utils/toast';
+import { MainWrapper } from '@/shared/components';
 
 import { NewsItem } from './ui/NewsItem';
 
 export const News = () => {
   return (
-    <MainWrapper title="Earn" page="earn">
+    <MainWrapper title="News" page="news">
       <div
         className="grid m-5 gap-14 overflow-y-scroll overflow-x-hidden h-full pt-7 "
         style={{ scrollbarWidth: 'none', width: 'calc(100% - 40px)' }}
@@ -16,9 +15,6 @@ export const News = () => {
         {news.map((item) => (
           <NewsItem key={item.id} item={item} />
         ))}
-      </div>
-      <div className="flex items-center justify-center h-20 w-full">
-        <Button onClick={() => toastInfo('scan re')} title="Scan receipt QR" />
       </div>
     </MainWrapper>
   );
