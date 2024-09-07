01cd442484758c7b261b4877d8de11f0aeca33c3
qutluq <kootlook.applications@gmail.com>
2024-09-07 18:27:39 +0500

    chore: remove easter egg on earn page

--- web/src/_pages/earn/Earn.tsx
+++ web/src/_pages/earn/Earn.tsx
@@ -1,21 +1,16 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { ReactNode, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import { Button, DoneMessage, GoBackButton, MainWrapper } from '@/shared/components';
+import { Button, MainWrapper } from '@/shared/components';
 import { CampaignState } from '@/shared/types';
 import { listCampaigns } from '@/shared/utils/rollup';
 
 import { Product } from './ui/Product';
 
-const Message = ({ children }: { children: ReactNode }) => {
-  return <DoneMessage>{children}</DoneMessage>;
-};
-
 export const Earn = () => {
   const router = useRouter();
-  const [isDone, setIsDone] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
   const [hasError, setHasError] = useState(false);
@@ -37,29 +32,11 @@ export const Earn = () => {
     void init();
   }, []);
 
-  if (isDone) {
-    return (
-      <MainWrapper title="Earn" page="earn" isError={hasError}>
-        <Message>
-          <div className="h-full w-full flex flex-col items-center justify-between py-3">
-            <div className="text-[35px] text-black text-center flex flex-col">
-              <span>You are now eligible</span>
-              <span>to receive a</span>
-              <span>&quot;Chocolate&quot;</span>
-            </div>
-            <GoBackButton handleClick={() => setIsDone(false)} />
-          </div>
-        </Message>
-      </MainWrapper>
-    );
-  }
-
   return (
-    <MainWrapper title="Earn" page="earn" isLoading={isLoading}>
+    <MainWrapper title="Earn" page="earn" isLoading={isLoading} isError={hasError}>
       <div
         className="flex m-5 gap-6 overflow-y-scroll overflow-x-hidden h-full flex-col items-start flex-grow"
         style={{ scrollbarWidth: 'none', width: 'calc(100% - 40px)' }}
-        onClick={() => setIsDone(true)}
       >
         {campaigns.map((product, idx) => (
           <Product key={`campaign-${idx}`} product={product} />
