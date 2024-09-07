0585470369122e2713721b80a336af2fedb423b3
Denis Perov <denis.perov@gmail.com>
2024-09-07 20:32:47 +0300

    Integrate blockchain calls to MRU endpoint

--- mru/src/api/MultiTokenERC20.js
+++ mru/src/api/MultiTokenERC20.js
@@ -1,76 +1,122 @@
-const ethers = require('ethers');
+import * as ethers from 'ethers';
 import MultiTokenERC20Definition from 'src/abi/MultiTokenERC20.json';
 
 // Replace with your contract's ABI
 const contractABI = MultiTokenERC20Definition.abi;
 
-// Replace with your deployed contract address
-const contractAddress = '0xYourContractAddressHere';
+const CHAIN_DATA = {
+  '2810': {
+    rpc: 'https://rpc-quicknode-holesky.morphl2.io',
+    address: '0x66D6B6483ec46abD950256880F39Ba56dc1b1a7f',
+    explorer: 'https://explorer-holesky.morphl2.io/tx',
+  },
+  '88882': {
+    rpc: 'https://spicy-rpc.chiliz.com',
+    address: '0x8c4F510229863854ecD6fB0d7161d1aB5336cc27',
+    explorer: 'https://testnet.chiliscan.com/tx',
+  },
+  '31': {
+    rpc: 'https://rpc.testnet.rootstock.io/L6ZhwA295IZEj8TgDgtnlNnoVumEws-T',
+    address: '0xaE7902b8050ef5204C74b5cbC5a2b91Ac6140D2d',
+    explorer: 'https://explorer.testnet.rsk.co/tx',
+  },
+  '296': {
+    rpc: 'https://testnet.hashio.io/api',
+    address: '0xdfAab04Fbe10E5f9AfB653e797EA8AE31ECaB715',
+    explorer: 'https://hashscan.io/testnet/tx',
+  },
+};
 
 // Function to get a contract instance
-function getContractInstance(signer) {
-  return new ethers.Contract(contractAddress, contractABI, signer);
+function getContractInstance(chainId) {
+  if (!(chainId in CHAIN_DATA)) {
+    throw new Error(`Unsupported chain ID: ${chainId}`);
+  }
+  const { rpc, address } = CHAIN_DATA[chainId];
+  const provider = ethers.getDefaultProvider(rpc);
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  return new ethers.Contract(address, contractABI, signer);
 }
 
 // Main interaction functions
-const MultiTokenERC20 = {
+export const MultiTokenERC20 = {
   // Create a new token
-  createToken: async (signer, name, symbol) => {
-    const contract = getContractInstance(signer);
+  createToken: async (chainId, name, symbol) => {
+    const contract = getContractInstance(chainId);
     const tx = await contract.createToken(name, symbol);
     const receipt = await tx.wait();
     console.log(`Token created: ${name} (${symbol})`);
     console.log(`Transaction hash: ${tx.hash}`);
-    return { receipt, transactionHash: tx.hash };
+    return {
+      receipt,
+      hash: tx.hash,
+      link: `${CHAIN_DATA[chainId].explorer}/${tx.hash}`,
+    };
   },
 
   // Mint tokens
-  mintTokens: async (signer, tokenName, to, amount) => {
-    const contract = getContractInstance(signer);
+  mintTokens: async (chainId, tokenName, to, amount) => {
+    const contract = getContractInstance(chainId);
     const tx = await contract.mintTokens(
       tokenName,
       to,
-      ethers.utils.parseEther(amount),
+      ethers.parseEther(amount),
     );
     const receipt = await tx.wait();
     console.log(`Minted ${amount} ${tokenName} tokens to ${to}`);
     console.log(`Transaction hash: ${tx.hash}`);
-    return { receipt, transactionHash: tx.hash };
+    return {
+      receipt,
+      hash: tx.hash,
+      link: `${CHAIN_DATA[chainId].explorer}/${tx.hash}`,
+    };
   },
 
   // Burn tokens
-  burnTokens: async (signer, tokenName, from, amount) => {
-    const contract = getContractInstance(signer);
+  burnTokens: async (chainId, tokenName, from, amount) => {
+    const contract = getContractInstance(chainId);
     const tx = await contract.burnTokens(
       tokenName,
       from,
-      ethers.utils.parseEther(amount),
+      ethers.parseEther(amount),
     );
     const receipt = await tx.wait();
     console.log(`Burned ${amount} ${tokenName} tokens from ${from}`);
     console.log(`Transaction hash: ${tx.hash}`);
-    return { receipt, transactionHash: tx.hash };
+    return {
+      receipt,
+      hash: tx.hash,
+      link: `${CHAIN_DATA[chainId].explorer}/${tx.hash}`,
+    };
   },
 
   // Get token balance
-  getBalance: async (signer, tokenName, address) => {
-    const contract = getContractInstance(signer);
+  getBalance: async (chainId, tokenName, address) => {
+    const contract = getContractInstance(chainId);
     const balance = await contract.balanceOf(tokenName, address);
-    return ethers.utils.formatEther(balance);
+    return ethers.formatEther(balance);
+  },
+
+  getTokenName: async (chainId, address) => {
+    //TODO: Implement
+    return {
+      '88882:0x7556403BaC4Bb71F09f3b3C2Fdb6f9C461CAe712': 'RCC',
+      '2810:0x4a68cb5b99b6e1db5745eeabde38c5b12aa0ce55': 'RCRC',
+    }[`${chainId}:${address}`];
   },
 
   // Get token address
-  getTokenAddress: async (signer, tokenName) => {
-    const contract = getContractInstance(signer);
+  getTokenAddress: async (chainId, tokenName) => {
+    const contract = getContractInstance(chainId);
     return await contract.tokens(tokenName);
   },
 
   // Approve token burn
-  approveTokenBurn: async (signer, tokenName, amount) => {
-    const contract = getContractInstance(signer);
+  approveTokenBurn: async (chainId, tokenName, amount) => {
+    const contract = getContractInstance(chainId);
     const tx = await contract.approveTokenBurn(
       tokenName,
-      ethers.utils.parseEther(amount),
+      ethers.parseEther(amount),
     );
     const receipt = await tx.wait();
     console.log(`Approved burn of ${amount} ${tokenName} tokens`);
--- mru/src/api/tokens.js
+++ mru/src/api/tokens.js
@@ -1,20 +1,96 @@
+import { last } from 'lodash';
 import { machine } from '../machine.js';
 import { makeActionHandler, server } from './server.js';
-import { ContractFactory } from 'ethers';
+import { MultiTokenERC20 } from './MultiTokenERC20.js';
+import { ActionConfirmationStatus } from '@stackr/sdk';
+import { rollup } from 'src/rollup.js';
+const { actions, getStfSchemaMap, submitAction } = rollup;
+const transitionToSchema = getStfSchemaMap();
 
+async function handleAction(name, req) {
+  const { msgSender, signature, inputs } = req.body;
+  const schemaId = transitionToSchema[name];
+  if (/* !transitions[name] ||  */ !schemaId) {
+    throw new Error('NO_TRANSITION_FOR_ACTION');
+  }
+  const schema = actions.getSchema(schemaId);
+  if (!schema) {
+    throw new Error('NO_SCHEMA_FOUND');
+  }
+  const signedAction = schema.actionFrom({
+    msgSender,
+    signature,
+    inputs,
+  });
+  const ack = await submitAction(name, signedAction);
+  const { logs, errors } = await ack.waitFor(ActionConfirmationStatus.C1);
+  if (errors?.length) {
+    throw new Error(errors[0].message);
+  }
+  return logs;
+}
 
 server.get('/mints', (req, res) => {
   res.status(200).send(machine.state.mints);
+});
+
+server.post('/mints', async (req, res) => {
+  try {
+    await handleAction('addReceipt', req);
+    const receipt = last(machine.state.receipts);
+    const items = await Promise.all(
+      receipt.mints.map(async (idx) => {
+        const mint = machine.state.mints[idx];
+        const [chainId, token] = mint.token.split(':');
+        const tokenName = await MultiTokenERC20.getTokenName(chainId, token);
+        return MultiTokenERC20.mintTokens(
+          chainId,
+          tokenName,
+          mint.customer,
+          mint.amount,
+        );
+      }),
+    );
+    return res.status(200).send({ items });
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
 });
 
 server.get('/burns', (req, res) => {
   res.status(200).send(machine.state.burns);
 });
 
-server.post('/tokens', (req, res) => {
-  const { chainId, symbol, allowance } = req.body;
-  //TODO: handle
-  return res.status(200).send({ address: '0x0' });
+server.post('/burns', async (req, res) => {
+  try {
+    await handleAction('claimReward', req);
+    const burns = machine.state.burns.slice(-2);
+    const items = await Promise.all(
+      burns.map(async (burn) => {
+        const [chainId, token] = burn.token.split(':');
+        const tokenName = await MultiTokenERC20.getTokenName(chainId, token);
+        return MultiTokenERC20.burnTokens(
+          chainId,
+          tokenName,
+          burn.customer,
+          burn.amount,
+        );
+      }),
+    );
+    return res.status(200).send({ items });
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+server.post('/tokens', async (req, res) => {
+  const { chainId, symbol } = req.body;
+  const { link, hash } = await MultiTokenERC20.createToken(
+    chainId,
+    symbol,
+    symbol,
+  );
+  return res.status(200).send({ link, hash });
 });
 
 server.post('/exchange', makeActionHandler('claimReward'));
