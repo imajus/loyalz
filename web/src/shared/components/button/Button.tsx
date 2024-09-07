ae931d1729f0abcf99f699653a61e4bd7879346d
qutluq <kootlook.applications@gmail.com>
2024-09-07 21:32:03 +0500

    feat: add size prop to shared button component

--- web/src/shared/components/button/Button.tsx
+++ web/src/shared/components/button/Button.tsx
@@ -4,6 +4,7 @@ import { ComponentProps, ReactNode } from 'react';
 import { classNames } from '@/shared/utils';
 
 type ButtonType = 'default' | 'grid';
+type ButtonSize = 'base' | 'md' | 'sm' | 'xs' | 'lg';
 
 type PropTypes = {
   title?: string;
@@ -13,27 +14,46 @@ type PropTypes = {
   className?: string;
   hasBorder?: boolean;
   btnType?: ButtonType;
+  btnSize?: ButtonSize;
 } & ComponentProps<'button'> &
   ComponentProps<'a'>;
 
-const getClassNames = (className: string | undefined) => {
+const getClassNames = (className: string | undefined, btnSize: ButtonSize) => {
   if (className) return className;
 
   const defaultClassName = classNames(
-    "text-black text-xl font-['Racing_Sans_One'] text-black text-3xl w-56 h-12 rounded-xl flex flex-row items-center justify-center cursor-pointer border-2 border-black shadow-sm",
+    "text-black text-xl font-['Racing_Sans_One'] text-black text-3xl h-12 rounded-xl flex flex-row items-center justify-center cursor-pointer border-2 border-black shadow-sm",
+    btnSize === 'lg' && 'w-72',
+    btnSize === 'base' && 'w-56',
+    btnSize === 'md' && 'w-48',
+    btnSize === 'sm' && 'w-32',
+    btnSize === 'xs' && 'w-24',
   );
 
   return defaultClassName;
 };
 
-const Shadow = ({ hasBorder, btnType }: { hasBorder: boolean; btnType: ButtonType }) => (
+const Shadow = ({
+  hasBorder,
+  btnType,
+  btnSize,
+}: {
+  hasBorder: boolean;
+  btnType: ButtonType;
+  btnSize: ButtonSize;
+}) => (
   <>
     {hasBorder && (
       <div
         className={classNames(
           'absolute inset-0 rounded-xl border-2 border-black shadow-[1px_1px_0_0_rgba(0,0,0,1)]',
           btnType === 'grid' && 'w-full',
-          btnType === 'default' && 'w-56',
+          btnType === 'default' && '',
+          btnSize === 'lg' && 'w-72',
+          btnSize === 'base' && 'w-56',
+          btnSize === 'md' && 'w-48',
+          btnSize === 'sm' && 'w-32',
+          btnSize === 'xs' && 'w-24',
         )}
       />
     )}
@@ -49,6 +69,7 @@ export const Button = (props: PropTypes) => {
     className,
     hasBorder = true,
     btnType = 'default',
+    btnSize = 'base',
     ...restProps
   } = props;
 
@@ -56,12 +77,12 @@ export const Button = (props: PropTypes) => {
   if (!title && !children) return null;
 
   const parentClassName = classNames('relative', btnType === 'grid' && 'w-full');
-  const childClassName = getClassNames(className);
+  const childClassName = getClassNames(className, btnSize);
 
   if (href) {
     return (
       <Link href={href} className={parentClassName} {...restProps}>
-        <Shadow hasBorder={hasBorder} btnType={btnType} />
+        <Shadow hasBorder={hasBorder} btnType={btnType} btnSize={btnSize} />
         <div className={childClassName}>
           {title}
           {children}
@@ -72,7 +93,7 @@ export const Button = (props: PropTypes) => {
 
   return (
     <button className={parentClassName} onClick={onClick} {...restProps}>
-      <Shadow hasBorder={hasBorder} btnType={btnType} />
+      <Shadow hasBorder={hasBorder} btnType={btnType} btnSize={btnSize} />
       <div className={childClassName}>
         {title}
         {children}
