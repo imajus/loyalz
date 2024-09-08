87aefaf2da604935d2234ceb030bede7c9638de7
qutluq <kootlook.applications@gmail.com>
2024-09-08 17:25:28 +0500

    feat: add loading spinner to retailer home

--- web/src/_pages/retailer-home/RetailerHome.tsx
+++ web/src/_pages/retailer-home/RetailerHome.tsx
@@ -6,9 +6,17 @@ import { ReactNode } from 'react';
 import { Button, Icon, RetailerWrapper } from '@/shared/components';
 import { useWeb3Auth } from '@/shared/hook';
 
-const Wrapper = ({ children, isError }: { children: ReactNode; isError: boolean }) => {
+const Wrapper = ({
+  children,
+  isError,
+  isLoading,
+}: {
+  children: ReactNode;
+  isError: boolean;
+  isLoading: boolean;
+}) => {
   return (
-    <RetailerWrapper title="Menu" isError={isError}>
+    <RetailerWrapper title="Menu" isError={isError} isLoading={isLoading}>
       <div className="w-full h-full flex flex-col justify-start items-center gap-7 pt-10">
         {children}
       </div>
@@ -17,19 +25,19 @@ const Wrapper = ({ children, isError }: { children: ReactNode; isError: boolean 
 };
 
 export const RetailerHome = () => {
-  const { isLoggedIn, loginWeb3Auth, web3user, isError } = useWeb3Auth();
+  const { isLoggedIn, loginWeb3Auth, web3user, isError, isLoading } = useWeb3Auth();
   const router = useRouter();
 
   if (!isLoggedIn || !web3user) {
     return (
-      <Wrapper isError={isError}>
+      <Wrapper isError={isError} isLoading={isLoading}>
         <Button onClick={() => loginWeb3Auth?.()} title="Login" />
       </Wrapper>
     );
   }
 
   return (
-    <Wrapper isError={isError}>
+    <Wrapper isError={isError} isLoading={isLoading}>
       <div className="flex flex-col gap-3 items-center justify-center">
         <Icon src={web3user.profileImage} />
         <span className="font-bold">{web3user.name}</span>
